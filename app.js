21b146856190276ecc53820613e0cb6220febfd0
Hatem Temimi <hatem.temimi@estale.fr>
2023-08-08 17:26:44 +0100

    fix: vercel

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
 //app.listen(process.env.PORT || 5000)
 app.listen(port, host, function() {
-  console.log("Server started.......");
+  console.log("Server started at port: ", port);
 });
 
 module.exports = app;
